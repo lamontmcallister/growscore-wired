a085a876b5e1e936d551c9e80fa308d5c382ba58
Lamont McAllister <lamontmcallister@Lamonts-MacBook-Pro.local>
2025-04-15 03:58:42 -0700

    <0001f9fc> Force update

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ st.set_page_config(page_title="Skippr", layout="wide")
 # Skippr App – One-Page Login Sidebar Integration
 import os
 import openai
-
 import ast
 import pdfplumber
 import pandas as pd
